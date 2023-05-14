e087adc4e1055424c6de70738be69cd85424fa72
noble-varghese <noblekvarghese96@gmail.com>
2023-05-14 17:02:50 +0530

    feat: exporting the functions as library methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
+pub mod style;
 pub mod align;
-pub mod border;
 pub mod color;
-pub mod join;
+pub mod border;
 pub mod padding;
 pub mod position;
 pub mod renderer;
-pub mod style;
+pub use color::Colour as Color;
 
 pub use border::Border;
 pub use border::{
     block_border, double_border, hidden_border, inner_half_block_border, normal_border,
     outer_half_block_border, rounded_border, thick_border,
 };
-pub use color::Colour as Color;
+
+pub mod join;
+pub use join::{join_horizontally, join_vertically};
